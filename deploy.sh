7b574d710b4c0821a2599c7471ea4a2f88dc15d7
franslourens <franslourens86@gmail.com>
2020-09-29 23:21:27 +0200

    k8s files

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
-docker build -t franslourens/nginx-laravel:latest -t franslourens/nginx-laravel:$SHA -f ./nginx/Dockerfile ./nginx
 docker build -t franslourens/php-laravel:latest -t franslourens/php-laravel:$SHA -f Dockerfile .
+docker build -t franslourens/nginx-laravel:latest -t franslourens/nginx-laravel:$SHA -f ./nginx/Dockerfile ./nginx
 
-docker push franslourens/nginx-laravel:latest
 docker push franslourens/php-laravel:latest
+docker push franslourens/nginx-laravel:latest
 
-docker push franslourens/nginx-laravel:$SHA
 docker push franslourens/php-laravel:$SHA
+docker push franslourens/nginx-laravel:$SHA
 
 kubectl apply -f k8s/database-persistent-volume-claim.yaml
 kubectl apply -f k8s/database-persistent-volume-claim.yaml
