85eb384c8870ba6f9b61df95ca7cf21d9bf3ab16
Frans Lourens <franslourens86@gmail.com>
2020-10-06 09:56:13 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -14,5 +14,7 @@ kubectl apply -f k8s/mysql-deployment.yaml
 kubectl apply -f k8s/ingress-service.yaml
 kubectl apply -f k8s/client-cluster-ip-service.yaml
 kubectl apply -f k8s/client-deployment.yaml
+kubectl apply -f k8s/memcache-cluster-ip-service.yaml
+kubectl apply -f k8s/memcache-deployment.yaml
 
 kubectl set image deployments/client-deployment fpm=franslourens/php-laravel:$SHA
